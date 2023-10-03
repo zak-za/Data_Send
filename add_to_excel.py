f567e5c45155b5746ddfc5cbbada1d19f47a4147
Zakariae El Kouch <zelkouch@emeal.nttdata.com>
2023-10-03 17:01:54 +0100

    add new

--- add_to_excel.py
+++ add_to_excel.py
@@ -20,16 +20,18 @@ df_jobs_diarios = pd.read_excel('ManualJobsNuevoTestamentoV4.xlsx' ,
                                 sheet_name='Jobs Diarios' ,
                                 skiprows=1,usecols=['Nombre', 'Proceso'])
 df = pd.read_excel(excel_file, sheet_name=sheet_name)
-
+#df_jobs[['JOBNAME', 'Proceso']] = df_jobs['Cargas de datos'].str.split(r'-|\s-\s|-\s|-\s|\s', expand=True)
+df_jobs['Proceso'] = df_jobs['Cargas de datos'].str.split(r'-|\s-\s|-\s|-\s', expand=True)[1]
+df_jobs['JOBNAME'] = df_jobs['Cargas de datos'].str.split(r'-|\s-\s|-\s|-\s', expand=True)[0]
                         #Manupilation Dataframe by week 
 #today_data=df[df['DATEPLAN'].dt.date == today]
 #df = df[(df['DATEPLAN'].dt.date >= start_date) & (df['DATEPLAN'].dt.date <= end_date)]
 print(df)
                          #Filtere Dataframe 
-print(df_jobs['Cargas de datos'])
+print(df_jobs)
 filtered_jobs_diarios = df_jobs_diarios[df_jobs_diarios['Nombre'].isin(df['JOBNAME'])]
 print(filtered_jobs_diarios)
-filtered_data = df[df['JOBNAME'].isin(df_jobs['Cargas de datos'])]
+filtered_data = df[df['JOBNAME'].isin(df_jobs['JOBNAME'])]
 print("Filtered data")
 print(filtered_data)
 missing_jobs_data = df[~df['JOBNAME'].isin(filtered_data['JOBNAME'])]
@@ -40,12 +42,14 @@ print("Final Data")
 
 def get_proceso(jobname):   
   if jobname in df_jobs_diarios['Nombre'].values:       
-    return df_jobs_diarios[df_jobs_diarios['Nombre'] == jobname]['Proceso'].iloc[0] 
+    return jobname +' - '+df_jobs_diarios[df_jobs_diarios['Nombre'] == jobname]['Proceso'].iloc[0]  
   elif   jobname in df_jobs.values:       
-    return df_jobs[df_jobs == jobname]['Proceso'].iloc[0]  
-  return None
+    return jobname +' - '+ df_jobs[df_jobs['JOBNAME'] == jobname]['Proceso'].iloc[0]  
+  return jobname
 
-final_data['Proceso'] = final_data['JOBNAME'].apply(get_proceso)
+#final_data['JOBNAME'] = final_data['JOBNAME']+' - '+final_data['JOBNAME'].apply(get_proceso)
+final_data['JOBNAME'] = final_data['JOBNAME'].apply(get_proceso)
 print(final_data)
-
+ 
+#final_data.to_excel('final_data.xlsx')
 #print(result_df)
